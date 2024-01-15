5d165db80ac7f193198f83b033a67e97b4a75d9e
Guy Sharony <40563195+guysharony@users.noreply.github.com>
2024-01-15 14:18:38 +0100

    ml03 - ex09

--- ml03/ex09/confusion_matrix.py
+++ ml03/ex09/confusion_matrix.py
@@ -1,8 +1,9 @@
 import numpy as np
+import pandas as pd
 from sklearn.metrics import confusion_matrix
 
 
-def confusion_matrix_(y_true, y_hat, labels=None):
+def confusion_matrix_(y_true, y_hat, labels=None, df_option=False):
     """
     Compute confusion matrix to evaluate the accuracy of a classification.
     Args:
@@ -18,8 +19,23 @@ def confusion_matrix_(y_true, y_hat, labels=None):
     Raises:
         This function should not raise any Exception.
     """
-    ... Your code ...
+    y_true = y_true.flatten()
+    y_hat = y_hat.flatten()
 
+    if labels is None:
+        labels = np.unique(np.concatenate((y_true, y_hat)))
+
+    num_labels = len(labels)
+    confusion_matrix = np.zeros((num_labels, num_labels), dtype=int)
+
+    for i, true_label in enumerate(labels):
+        for j, pred_label in enumerate(labels):
+            confusion_matrix[i, j] = np.sum((y_true == true_label) & (y_hat == pred_label))
+
+    if df_option:
+        return pd.DataFrame(confusion_matrix, index=labels, columns=labels)
+
+    return confusion_matrix
 
 if __name__ == "__main__":
     y_hat = np.array([['norminet'], ['dog'], ['norminet'], ['norminet'], ['dog'], ['bird']])
@@ -33,3 +49,11 @@ if __name__ == "__main__":
     # Example 2:
     print(f"<- {confusion_matrix_(y, y_hat, labels=['dog', 'norminet'])}")
     print(f"-> {confusion_matrix(y, y_hat, labels=['dog', 'norminet'])}")
+
+    # Example 3:
+    print(confusion_matrix_(y, y_hat, df_option=True))
+    print()
+
+    # Example 4:
+    print(confusion_matrix_(y, y_hat, labels=['bird', 'dog'], df_option=True))
+    print()
